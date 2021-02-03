85ee3fa8b82c7f70f18ff0b73ef56adad3007d3b
Alwar <alvaro.sobru@gmail.com>
2021-02-03 10:39:18 +0100

    Estructura de proyecto

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Aplicación de gestión de discos',
+    'description': 'Gestiona discos, grupos, artistas, canciones, etc..',
+    'author': ['Guillermo Bort', 'Nacho Sanz', 'Álvaro Sobrino'],
+    'depends': True,
+    'installable': True,
+    'data': [
+        'security/disco_security.xml'
+        'views/disco_view.xml'
+    ],
+    'demo': [],
+}
---
+++ models/__init__.py
@@ -0,0 +1,2 @@
+from . import musica_disco
+from . import musica_grupo
