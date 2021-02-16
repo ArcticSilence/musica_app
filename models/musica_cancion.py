367a45f18ddae05eb610eabd0b87d9fcff8b9bc4
odoo <nachosanz322@gmail.com>
2021-02-16 09:09:44 +0100

    A ver si ahora sí que están los modelos

---
+++ models/musica_cancion.py
@@ -0,0 +1,23 @@
+from odoo import fields, models
+
+
+class Cancion(models.Model):
+    # Ignacio Sanz
+    _name = 'musica.cancion'
+    _description = 'Cancion'
+
+    # String fields:
+    name = fields.Char(
+        'Titulo',
+        default=None,
+        index=True,
+        help='Título de la cancion',
+        readonly=False,
+        required=True,
+        translate=False,
+    )
+
+    # Numeric fields:
+    cod = fields.Integer
+    duracion = fields.Float((15, 5))
+
